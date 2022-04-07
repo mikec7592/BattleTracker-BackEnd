e41e302ceefaadca5b45a18f3e5998f2b532513d
michael clark <michaelclark@michaels-MacBook-Air.local>
2022-04-07 13:18:54 -0700

    wired signup route up to database to save user and password with hashing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+require('./models/User')
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes')
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(authRoutes);
 
 
 const mongoUri = 'mongodb+srv://mikec7592:garbage1@battle-tracker.skc78.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
